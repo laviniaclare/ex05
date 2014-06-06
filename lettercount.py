07bd1ebcd8ee0ed49854fe7288f3df40ddb74849
Lavinia Karl <triciaroth@Tricia-Roths-MacBook.local>
2014-06-06 14:59:07 -0700

    Made function to read the tex and function to count the letters.  Now creating function to print tallies in correct output format.

--- lettercount.py
+++ lettercount.py
@@ -1,14 +1,23 @@
-def put_text_into_list():
+def read_text():
     f = open("myfile.txt")
     filetext = f.read()
     char_list=list(filetext)
     char_list_lower=char_list.lower
     return char_list_lower
 
-def parse_text(char_list_lower):
-    a = 0
+def count_letters(char_list_lower):
+	letter_tallies=[]
     alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     for letter in alphabet:
-        char_list_lower.count(letter)
+        total=char_list_lower.count(letter)
+        letter_tallies.append(total)
+
+def print_letters():
+	
+
          
 def main():
+
+
+if __name__ == '__main__':
+	main()
